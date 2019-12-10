07a93b7ef6b65bf6f2635a511481f3956b46d309
Benji Lewis <benji@benjisoft.org.uk>
2019-12-10 12:53:20 +0000

    Working

---
+++ index.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require("./config.json");
+var danCount = 0;
+var camCount = 0;
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === '!mutedan') {
+      if(danCount == 5) {
+          msg.channel.send("?mute <@579965584872177665> 2 Public Opinion")
+          danCount = 0;
+      } else {
+          console.log("Mute triggered" + danCount);
+          danCount += 1;
+          msg.channel.send("Vote has been counted! Currently there are " + danCount + " votes. ")
+      }
+  }
+});
+
+client.login(config.token);
