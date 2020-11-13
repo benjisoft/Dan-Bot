2e4d28740133373e70853fc1cb295a79e69beb96
Oliver Beardsall <oli@beardsall.dev>
2020-11-13 17:18:22 +0000

    Added working code

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const aws = require('aws-sdk');
 const client = new Discord.Client();
 client.timeout = 130000
 var reqCount = 0;
+var voteCounts = {};
+
 
 // Added for local testing
 require('dotenv').config()
@@ -13,20 +15,29 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (!msg.content.startsWith('!votemute') || msg.author.bot) return;
-    global.args = msg.content.slice(8).trim();
-  
-    if(reqCount == 4) {
+  if (!msg.content.startsWith('!testvotemute') || msg.author.bot) return;
+    global.args = msg.content.slice(14).trim();
+
+    
+
+    if(voteCounts[args] == 4) {
       let member = msg.mentions.members.first();
       member.roles.add('652102004654735380').catch(console.error);
       setTimeout(function(){member.roles.remove('652102004654735380').catch(console.error)}, 120000, 'funky');
-      msg.channel.send(args + " has been muted")
-      reqCount = 0;
+      msg.channel.send(args + " has been muted");
+      console.log("Mute voted " + voteCounts[args] + "/5");
+      console.log(args + " has been muted");
+      voteCounts[args] = 0;
     }
     else {
-      console.log("Mute voted " + reqCount);
-      reqCount += 1;
-      msg.channel.send("Vote has been counted! Currently there are " + reqCount + " votes. ")
+      if (voteCounts.hasOwnProperty(args)) {
+        voteCounts[args] += 1;
+      }
+      else {
+        voteCounts[args] = 0;
+      }
+      console.log("Mute voted for " + args + voteCounts[args] + "/5");
+      msg.channel.send("Vote has been counted! Currently there are " + reqCount + " votes. ");
     }
     });
 
