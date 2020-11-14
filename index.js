8de6fe1d1ae79a8038d420e4faaef6ea8c72bc60
Restyled.io <commits@restyled.io>
2020-11-14 11:04:55 +0000

    Restyled by prettier

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-const Discord = require('discord.js');
-const aws = require('aws-sdk');
+const Discord = require("discord.js");
+const aws = require("aws-sdk");
 const client = new Discord.Client();
-client.timeout = 130000
+client.timeout = 130000;
 var voteCounts = {};
 
 // Added for local testing
-require('dotenv').config()
+require("dotenv").config();
 
-client.on('ready', () => { console.log(`Logged in as ${client.user.tag}!`); });
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
-client.on('message', msg => {
-  if (!msg.content.startsWith('!votemute') || msg.author.bot)
-    return;
+client.on("message", (msg) => {
+  if (!msg.content.startsWith("!votemute") || msg.author.bot) return;
   global.args = msg.content.slice(9).trim();
 
   if (args == "") {
     msg.channel.send("Please give someone to vote to mute.");
   } else if (voteCounts[args] == 4) {
     let member = msg.mentions.members.first();
-    member.roles.add('652102004654735380').catch(console.error);
-    setTimeout(function() {
-      member.roles.remove('652102004654735380').catch(console.error)
-    }, 120000, 'funky');
+    member.roles.add("652102004654735380").catch(console.error);
+    setTimeout(
+      function () {
+        member.roles.remove("652102004654735380").catch(console.error);
+      },
+      120000,
+      "funky"
+    );
     msg.channel.send(args + " has been muted");
     voteCounts[args] += 1;
     console.log("Mute voted for " + args + " " + voteCounts[args] + "/5");
@@ -34,8 +39,11 @@ client.on('message', msg => {
       voteCounts[args] = 0;
     }
     console.log("Mute voted for " + args + " " + voteCounts[args] + "/5");
-    msg.channel.send("Vote has been counted! Currently there are " +
-                     voteCounts[args] + " votes. ");
+    msg.channel.send(
+      "Vote has been counted! Currently there are " +
+        voteCounts[args] +
+        " votes. "
+    );
   }
 });
 
